88f118c51cd4f4c947c4c17b7d7a5c9c4a65f953
hmweb77 <oussamahamdani1@gmail.com>
2024-09-28 16:54:07 +0100

    modify favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Leadership Coach",
+  title: "ivanmoreira",
   description: "Leadership coach",
 };
 
